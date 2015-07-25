7a0667f40da3cf8d37cedd686693a07190cc26a0
Yuri Kovalov <yuri@yurikoval.com>
2015-07-25 15:54:01 +0900

    Handle urls that failed to scan

--- importer.rb
+++ importer.rb
@@ -5,6 +5,9 @@ require 'open-uri'
 require 'byebug'
 require 'csv'
 
+class XPathScanError < Exception
+end
+
 class Importer
   def self.scrape(args)
     instance = new args
@@ -15,6 +18,7 @@ class Importer
     @config = args.delete(:config)
     @url_set = args.delete(:url_set)
     @write_to = args.delete(:write_to)
+    @failed_url_set = []
   end
 
   def scrape
@@ -22,12 +26,21 @@ class Importer
       data = extract_data(url)
       write_to_file(data)
     end
+    print_failed_scans if @failed_url_set.any?
   end
   private
     def extract_data(url)
-      puts "Fetching #{url}"
-      doc = Nokogiri::HTML(open(url))
-      doc.xpath(@config['extraction']['resultXPaths'].first).map.with_index(1) do |result, i|
+      begin
+        puts "Fetching #{url}"
+        doc = Nokogiri::HTML(open(url))
+        results = doc.xpath(@config['extraction']['resultXPaths'].first)
+        raise XPathScanError, "No results in #{url}" if results.empty?
+      rescue OpenURI::HTTPError, XPathScanError => e
+        puts "Failed to fetch #{url}"
+        puts e.message
+        @failed_url_set << url
+      end
+      results.map.with_index(1) do |result, i|
         subjects.map do |subject|
           if match = result.xpath(subject[:xpath]).first
             returning = [match.content]
@@ -57,6 +70,13 @@ class Importer
     def write_to_file(data)
       CSV.open(@write_to, "a") do |csv|
         data.each{ |row| csv << row }
+      end
+    end
+
+    def print_failed_scans
+      puts "Failed to scan:"
+      @failed_url_set.each do |url|
+        puts "\t\t#{url}"
       end
     end
 end
