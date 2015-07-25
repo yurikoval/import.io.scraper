8103086011374927ab7827a3ea4f5232d5278f35
Yuri Kovalov <yuri@yurikoval.com>
2015-07-25 14:50:13 +0900

    Allow for scanning for certain page

--- import.rb
+++ import.rb
@@ -33,7 +33,8 @@ what_to_scrape = [
 ]
 
 what_to_scrape.each do |subject|
-  url_set = (1...(subject[:pages])).map{ |num| "#{subject[:url]}#{num}" }
+  start_at = subject[:start_at] || 1
+  url_set = ((start_at)...(subject[:pages])).map{ |num| "#{subject[:url]}#{num}" }
   date = Time.new().strftime('%Y-%m-%d')
   Importer.scrape config: config, url_set: url_set, write_to: "export_#{subject[:name]}_#{date}.txt"
 end
